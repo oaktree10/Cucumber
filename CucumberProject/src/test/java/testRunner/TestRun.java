be49b63efa94e0fb58d5669f3adbb64fd0cb38ac
Rahul <iamrvb00@gmail.com>
2024-07-22 17:56:08 +0530

    Test

---
+++ CucumberProject/src/test/java/testRunner/TestRun.java
@@ -0,0 +1,36 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+					//features= {".//Features/"},
+					//features= {".//Features/Login.feature"},
+					//features= {".//Features/Registration.feature"},
+		
+					//features= {".//Features/LoginDDTExcel.feature"},
+					features= {".//Feature/Login.feature",".//Feature/Registration.feature"},
+					//features= {"@target/rerun.txt"},
+					glue={"step","Hooks"},
+					plugin= {
+								"pretty", "html:reports/myreport.html",   
+								"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+								"rerun:target/rerun.txt",
+							
+							},
+							
+					dryRun=false,    // checks mapping between scenario steps and step definition methods
+					monochrome=true,    // to avoid junk characters in output
+					publish=true,   // to publish report in cucumber server
+					tags="@sanity"  // this will execute scenarios tagged with @sanity
+					//tags="@regression"
+					//tags="@sanity and @regression" //Scenarios tagged with both @sanity and @regression
+					//tags="@sanity and not @regression" //Scenarios tagged with @sanity but not tagged with @regression
+					//tags="@sanity or @regression" //Scenarios tagged with either @sanity or @regression
+		)
+public class TestRun {
+
+}
